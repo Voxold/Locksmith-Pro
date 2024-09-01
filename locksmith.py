724a25bd27063a80cece33aa697a7707a9954c8c
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2024-09-01 07:47:23 +0300

    new routs

--- locksmith.py
+++ locksmith.py
@@ -7,3 +7,20 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     return '<h1> Hello Locksmith </h1>'
+
+
+@app.route('/about')
+def home():
+    return '<h1> Hello Locksmith </h1>'
+
+@app.route('/contact')
+def home():
+    return '<h1> Contact us </h1>'
+
+@app.route('/privacy')
+def home():
+    return '<h1> Privacy policy </h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
